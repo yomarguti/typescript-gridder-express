21eb8076e5dae7be2fb3310010f3f204e44a2d52
yomarguti <yomar.guti@gmail.com>
2022-01-05 08:39:37 -0500

    Initial commit. Initial express setup with one route

---
+++ src/routes/login.ts
@@ -0,0 +1,15 @@
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.send(
+    `
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+    `
+  );
+});
+
+export { router };
